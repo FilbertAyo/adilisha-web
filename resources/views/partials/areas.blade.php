750bb0ddba5ba9f9df49ab4bc8625614d01a7048
FilbertAyo <filbertayo09@gmail.com>
2026-01-23 11:49:53 +0300

    video

--- resources/views/partials/areas.blade.php
+++ resources/views/partials/areas.blade.php
@@ -1,12 +1,14 @@
-<section class="ftco-section bg-light" style="position: relative; overflow: hidden; min-height: 80vh; padding: 100px 0;">
-  <!-- Background Video -->
-  <video autoplay muted loop playsinline style="position: absolute; top: 50%; left: 50%; min-width: 100%; min-height: 100%; width: auto; height: auto; z-index: 0; transform: translate(-50%, -50%); object-fit: cover; opacity: 0.9;">
-    <source src="{{ asset('front-end/images/WEBSITE.mp4') }}" type="video/mp4">
-  </video>
-  
-  <div class="container-fluid" style="position: relative; z-index: 1;">
+<style>
+  .areas-video {
+    width: 100%;
+    height: auto;
+    display: block;
+    margin: 0;
+    padding: 0;
+  }
+</style>
 
-
-
-  </div>
-</section>
+<!-- Background Video -->
+<video autoplay muted loop playsinline class="areas-video">
+  <source src="{{ asset('front-end/images/WEBSITE.mp4') }}" type="video/mp4">
+</video>
